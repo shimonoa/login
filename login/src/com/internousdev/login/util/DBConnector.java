fc22fefaa61cb589e53590a10922b61ada74fa02
shimonoa <oosubame315@yahoo.co.jp>
2020-05-25 19:33:55 +0900

    ログインする

---
+++ login/src/com/internousdev/login/util/DBConnector.java
@@ -0,0 +1,32 @@
+package com.internousdev.login.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBConnector {
+	private static String drivarName = "com.mysql.jdbc.Driver";
+	private static String url = "jdbc:mysql://localhost/logindb_shimono";
+
+	private static String user = "root";
+	private static String password ="";
+
+	public Connection getConnection() {
+		Connection con = null;
+
+		try {
+			Class.forName(drivarName);
+			con = (Connection)DriverManager.getConnection(url,user,password);
+
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+
+	}
+
+
+}
